dbb9790ff5525c2f29e0b1351ccd945b37d02143
Matteo Sasso <matteo.sasso@gmail.com>
2019-05-31 15:37:37 +0200

    New version
    - Fixed syntax error
    - Introduced environments separation
    - Only one HTTP request instead of two
    - bot is now a global variable
    - Refactoring

--- service.py
+++ service.py
@@ -10,69 +10,70 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0',
 }
 
-def handler(event, context):
-    bot = telegram.Bot(token=os.environ['telegram_token'])
+bot = telegram.Bot(token=os.environ['telegram_token'])
+
+def send_message(message, bot=bot):
+    if os.environ['app_env'] != "prod": return print(message)
+    
+    bot.send_message(chat_id=os.environ['telegram_channel'], text=message)
+
+def send_description_if_new(description):
+    if os.environ['app_env'] != "prod": return print(description)
     
     try:
-        page = requests.get('https://cisecurity.org', headers=headers)
-    except Exception, e:
-        bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't load main page"))
-        raise e
-        
-    try:
-        tree = html.fromstring(page.content)
-    except Exception, e:
-        bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't parse main page"))
-        raise e
-
-    try:
-        level = tree.xpath('//div[contains(concat(" ", @class, " "), " alertLevel ")]//span[contains(concat(" ", @class, " "), " text-primary ")]/text()')[0]
-    except Exception, e:
-        bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't find element in main page"))
-        raise e
-
-    if level in ('LOW', 'GREEN'):
-        return 0
-    else:
-        bot.send_message(chat_id=os.environ['telegram_channel'], text=("WARNING: cyber threat level is " + level))
-        try:
-            page2 = requests.get('https://www.cisecurity.org/cybersecurity-threats/', headers=headers)
-        except Exception, e:
-            bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't load description page"))
-            raise e
-
-        try:
-            tree2 = html.fromstring(page2.content)
-        except Exception, e:
-            bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't parse description page"))
-            raise e
-
-        try:
-            description = tree2.xpath('//div[@class="secure-platforms"]//p[@class="read-security-details"]/text()')[-1].strip()
-        except Exception, e:
-            bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't find element in description page"))
-            raise e
-            
-        try:
-            dynamodb = boto3.resource('dynamodb')
-            table = dynamodb.Table('cis_threat_dedup')
-            response = table.get_item(
-                Key={
-                    'description': description,
-                }
-            )
-            if response.get('Item', None) and response['Item']['description'] == description:
-                return 0
-        except Exception, e:
-            bot.send_message(chat_id=os.environ['telegram_channel'], text=("CIS threat ERROR, couldn't query DynamoDB"))
-            raise e
-            
-        bot.send_message(chat_id=os.environ['telegram_channel'], text=(description))
-        if response.get('Item', None):
-            table.delete_item(Key=response['Item'])
-        table.update_item(
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('cis_threat_dedup')
+        response = table.get_item(
             Key={
                 'description': description,
             }
         )
 
+    except Exception as e:
+        send_message("CIS threat ERROR, couldn't query DynamoDB")
+        raise e
+        
+    if response.get('Item', None) and response['Item']['description'] == description:
+        send_message(description)
+        
+    if response.get('Item', None):
+        table.delete_item(Key=response['Item']['description'])
+        
+    table.update_item(
+        Key={
+            'description': description,
+        }
+    )
+ 
+
+def handler(event, context):
+    try:
+        page = requests.get('https://www.cisecurity.org/cybersecurity-threats/', headers=headers)
+    except Exception as e:
+        send_message("CIS threat ERROR, couldn't load CIS page")
+        raise e
+        
+    try:
+        tree = html.fromstring(page.content)
+    except Exception as e:
+        send_message("CIS threat ERROR, couldn't parse CIS page")
+        raise e
+
+    try:
+        level = tree.xpath('//div[contains(concat(" ", @class, " "), " alert-level ")]//span[contains(concat(" ", @class, " "), " text-primary ")]/text()')[0]
+    except Exception as e:
+        send_message("CIS threat ERROR, couldn't find element in main page")
+        raise e
+
+    if level in ('LOW', 'GREEN'):
+        return 0
+    else:
+        send_message("WARNING: cyber threat level is " + level)
+
+        try:
+            description = tree.xpath('//div[@class="secure-platforms"]//p[@class="read-security-details"]/text()')[-1].strip()
+        except Exception as e:
+            send_message("CIS threat ERROR, couldn't find element in CIS page")
+            raise e
+            
+        send_description_if_new(description)
